ca76b1594feb667d1cd71585e82f20494ba489e5
ReylanTeo <109081015+ReylanTeo@users.noreply.github.com>
2023-10-24 17:19:58 +0800

    Hello

--- src/tui/component/keyboard.js
+++ src/tui/component/keyboard.js
@@ -1,6 +1,7 @@
 const term = require('terminal-kit').terminal;
 
 const showMenu = (items) => new Promise((resolve, reject) => {
+    items.push("Done")
     term.singleColumnMenu(items, (error, response) => {
         if (error) {
             reject(error);
@@ -10,36 +11,39 @@ const showMenu = (items) => new Promise((resolve, reject) => {
     });
 });
 
-(async () => {
-    console.log('Testing Menu.');
-    // Array options
-    const items = ['a. 1', 'b. 2', 'c. 3', 'Done'];
-    try {
-        const response = await showMenu(items);
-        // Options
-        term.eraseLineAfter.cyan(
-            `#${response.selectedIndex} selected: ${response.selectedText} (${response.x},${response.y})\n`
-        );
-        if (response.selectedText == "Done") {
-            console.log("Hello World");
-        }
-    } catch (error) {
-        console.error('Error:', error);
-    } finally {
-        process.exit();
-    }
-})();
-
 const terminate = async () => {
     term.grabInput(false);
-    // Clear the screen
     term.clear();
     await new Promise(resolve => setTimeout(() => resolve(), 100));
     process.exit();
 };
 
-term.on('key', (name, matches, data) => {
+term.on('key', async (name, matches, data) => {
     if (name === 'CTRL_C') {
-        terminate();
+        await terminate();
     }
 });
+
+const TerminalUI = {
+    SingleChoiceMenu: async (items) => {
+        console.log('Testing Menu.');
+        try {
+            const response = await showMenu(items);
+            term.eraseLineAfter.cyan(
+                `#${response.selectedIndex} selected: ${response.selectedText} (${response.x},${response.y})\n`
+            );
+            if (response.selectedText !== "Done") {
+                console.log("Exit Program");
+            }
+        } catch (error) {
+            console.error('Error:', error);
+        } finally {
+            process.exit();
+        }
+    },
+    MultipleChoiceMenu: () => {
+        console.log("Hello World");
+    }
+};
+
+TerminalUI.SingleChoiceMenu(["Hello", "World"])
