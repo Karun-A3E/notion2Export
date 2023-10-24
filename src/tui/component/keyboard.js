d9711a61b3736a300cdd55b9dedbd6efd655fdfd
ReylanTeo <109081015+ReylanTeo@users.noreply.github.com>
2023-10-24 16:03:33 +0800

    H

--- src/tui/component/keyboard.js
+++ src/tui/component/keyboard.js
@@ -1,44 +1,45 @@
 const term = require('terminal-kit').terminal;
 
 const showMenu = (items) => new Promise((resolve, reject) => {
-  term.singleColumnMenu(items, (error, response) => {
-    if (error) {
-      reject(error);
-    } else {
-      resolve(response);
-    }
-  });
+    term.singleColumnMenu(items, (error, response) => {
+        if (error) {
+            reject(error);
+        } else {
+            resolve(response);
+        }
+    });
 });
 
 (async () => {
-  //array options
-  const items = ['a. 1', 'b. 2', 'c. 3'];
-  console.log('Testing Menu.');
-  try {
-    const response = await showMenu(items);
-    //Options
-    term.eraseLineAfter.cyan(
-      `#${response.selectedIndex} selected: ${response.selectedText} (${response.x},${response.y})\n`
-    );
-  } catch (error) {
-    console.error('Error:', error);
-  } finally {
-    process.exit();
-  }
+    console.log('Testing Menu.');
+    // Array options
+    const items = ['a. 1', 'b. 2', 'c. 3', 'Done'];
+    try {
+        const response = await showMenu(items);
+        // Options
+        term.eraseLineAfter.cyan(
+            `#${response.selectedIndex} selected: ${response.selectedText} (${response.x},${response.y})\n`
+        );
+        if (response.selectedText == "Done") {
+            console.log("Hello World");
+        }
+    } catch (error) {
+        console.error('Error:', error);
+    } finally {
+        process.exit();
+    }
 })();
 
 const terminate = async () => {
     term.grabInput(false);
-    term.clear(); // Clear the screen
+    // Clear the screen
+    term.clear();
     await new Promise(resolve => setTimeout(() => resolve(), 100));
     process.exit();
-  };
-  
-  term.on('key', (name, matches, data) => {
+};
+
+term.on('key', (name, matches, data) => {
     if (name === 'CTRL_C') {
-    //   console.log("'key' event:", name);
-      terminate();
+        terminate();
     }
-  });
-  
-  
+});
